e97b1c6ab92a6d71369618cd36a73e466ece7339
llcc01 <2417224420@qq.com>
2025-11-03 13:51:44 +0800

    fix(base64): urlSafeBase64

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,6 @@
 export const base64Decode = (d: string) => {
-  const data = atob(d);
+  const base64 = d.replace(/-/g, "+").replace(/_/g, "/");
+  const data = atob(base64);
   const decoder = new TextDecoder();
   return decoder.decode(
     new Uint8Array([...data].map((char) => char.charCodeAt(0)))
